7673d83b5b1133e7055c2f6c032bbd331d3d143c
hywang <302747091@qq.com>
2021-08-04 12:38:02 +0800

    Update do_all_BC4.sh

--- TVNet-ANET/do_all_BC4.sh
+++ TVNet-ANET/do_all_BC4.sh
@@ -16,14 +16,14 @@ module load languages/anaconda2/5.0.1.tensorflow-1.6.0
 python TEM_train.py
 python TEM_test.py
 
-python VEM_create_windows.py --window_length 15 --window_stride 3
+python VEM_create_windows.py --window_length 15 --window_stride 5
 python VEM_create_windows.py --window_length 5 --window_stride 2
 
-python VEM_train.py --voting_type start --window_length 15 --window_stride 3
-python VEM_test.py --voting_type start --window_length 15 --window_stride 3
+python VEM_train.py --voting_type start --window_length 15 --window_stride 5
+python VEM_test.py --voting_type start --window_length 15 --window_stride 5
 
-python VEM_train.py --voting_type end --window_length 15 --window_stride 3
-python VEM_test.py --voting_type end --window_length 15 --window_stride 3
+python VEM_train.py --voting_type end --window_length 15 --window_stride 5
+python VEM_test.py --voting_type end --window_length 15 --window_stride 5
 
 python VEM_train.py --voting_type start --window_length 5 --window_stride 2
 python VEM_test.py --voting_type start --window_length 5 --window_stride 2
